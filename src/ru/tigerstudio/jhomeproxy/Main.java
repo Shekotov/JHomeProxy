780eca783ab248bd022dd822ed74676042e94232
Shekotov <pavel-shekotov@mail.ru>
2017-06-14 07:48:52 +0300

    Start JHomeProxy project

---
+++ src/ru/tigerstudio/jhomeproxy/Main.java
@@ -0,0 +1,17 @@
+package ru.tigerstudio.jhomeproxy;
+
+/**
+ *
+ * @author Tiger
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
